879b8e367545d92d6cc075803edd23977532c663
ssbpgm <pagm.tech@gmail.com>
2024-05-10 01:40:27 -0500

    second attempt

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -18,19 +18,57 @@ import java.util.Map;
 )
 @LambdaUrlConfig(authType = AuthType.NONE,
 	invokeMode = InvokeMode.BUFFERED)
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
+	public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
+
 		System.out.println("Hello from lambda");
+
+		// D: to know how the payload is structured see https://docs.aws.amazon.com/lambda/latest/dg/urls-invocation.html#urls-request-payload
+
+		// Get the details from the payload
+		Map<String, Object> payloadRequestContext = (Map<String, Object>) request.get("requestContext");
+
+		String domainName = (String) payloadRequestContext.get("domainName");
+		System.out.println("Domain Name: " + domainName);
+
+		Map<String, Object> payloadRequestContextHttp = (Map<String, Object>) payloadRequestContext.get("http");
+		String httpMethod = (String) payloadRequestContextHttp.get("httpMethod");
+		System.out.println("HTTP Method: " + httpMethod);
+
+		// path is the part starting from '/' coming after the domain name
+		String path = (String) payloadRequestContextHttp.get("path");
+		System.out.println("Path: " + path);
+
+
+		// Construct the URL
+		String url = "https://" + domainName + path;
+		System.out.println("URL: " + url);
+
 		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		//resultMap.put("message", "Hello from Lambda");
-		// since it's going to be called like and endpoint, we need to return a JSON object
-		resultMap.put("body",
-				"{" +
-				"\"statusCode\": 200," +
-				"\"message\": \"Hello from Lambda\"" +
-				"}");
+
+		if (path.equals("/hello")) {
+			resultMap.put("statusCode", 200);
+
+			// since it's going to be called like and endpoint, we need to return a JSON object
+			resultMap.put("body",
+					"{" +
+							"\"statusCode\": 200," +
+							"\"message\": \"Hello from Lambda\"" +
+							"}");
+		}
+		else {
+			resultMap.put("statusCode", 400);
+
+			// since it's going to be called like and endpoint, we need to return a JSON object
+			resultMap.put("body",
+					"{" +
+							"\"statusCode\": 400," +
+							"\"message\": \"Bad request syntax or unsupported method. Request path: /cmtr-3ff04848. HTTP method: GET\"" +
+							"}");
+
+		}
+
 		return resultMap;
 	}
 }
